4a4a9fff82ec870c307ea9bfd7cf213023c3b957
Malik Henry <mhenry30658@gmail.com>
2022-05-23 19:32:35 -0400

    initialized schema and seeds.

---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO department (id, name)
+VALUES  (001, "Sales")
+        (002, "Legal")
+        (003, "Engineering")
+        (004, "Finance")
+        
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES  (001, "Salesperson", "150000", 001)
+        (002, "Attorney", "250000", 002)
+        (003, "Software Engineer", "190000", 003)
+        (004, "CFO", "250000", 004)
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES  (001, "BillyBob", "Wilson", 001, null)
+        (002, "Jan", "Firster", 002, null)
+        (003, "Marko", "Lopez", 003, 001)
+        (004, "Taryn", "O'Haire", 004, 001)
